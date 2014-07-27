8392bd4019e3c1ae155fa37799d4d4a41dfa2830
Jessica Kerr <jessitron@gmail.com>
2014-07-27 10:33:38 -0500

    One failing example test

---
+++ lib/rules.rb
@@ -0,0 +1,10 @@
+
+class Rules
+  def initialize (temptation_to_defect, reward_for_mutual_cooperation, punishment_for_mutual_defection, suckers_payout)
+
+  end
+
+  def score(move1, move2)
+    [1,1]
+  end
+end
---
+++ spec/rules_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/rules'
+
+describe Rules do
+
+  context "suckers interact with each other" do
+    subject { described_class.new(5, 3, 2, 1)  }
+
+    it "scores the same for each player" do
+      expect(subject.score(:cooperate, :cooperate)).to eq([3, 3])
+    end
+  end
+end
