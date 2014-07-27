71519be4a2c152f5caf121d0da7e2d4ad431bf14
Jessica Kerr <jessitron@gmail.com>
2014-07-27 10:36:12 -0500

    technically green

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,10 +1,11 @@
 
 class Rules
-  def initialize (temptation_to_defect, reward_for_mutual_cooperation, punishment_for_mutual_defection, suckers_payout)
 
+  def initialize (temptation_to_defect, reward_for_mutual_cooperation, punishment_for_mutual_defection, suckers_payout)
+    @r = reward_for_mutual_cooperation
   end
 
   def score(move1, move2)
-    [1,1]
+    [@r,@r]
   end
 end
