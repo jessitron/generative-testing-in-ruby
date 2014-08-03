4484dcb02cd54f21d4da76e91313db8c65ce9730
Jessica Kerr <jessitron@gmail.com>
2014-08-03 12:14:00 -0500

    before switching test frameworks

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -12,9 +12,12 @@ describe Rules do
 
   generative do
     data(:rules) { Rules.new(5,3,2,1) }
+    data(:opponent_move) { :cooperate }
 
-    it ("does something") do
-      expect(rules.score(:cooperate, :cooperate)).to eq([3,3])
+    it ("defection always better for me") do
+      defection_score = rules.score(:defect, opponent_move).first
+      cooperation_score = rules.score(:cooperate, opponent_move).first
+      expect(defection_score).to be > cooperation_score
     end
   end
 end
