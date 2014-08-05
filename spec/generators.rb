09227df1f4b9e69d193b449fdb1cbcf8ee46214b
Jessica Kerr <jessitron@gmail.com>
2014-08-04 21:42:13 -0500

    quick, it generated something.

--- spec/generators.rb
+++ spec/generators.rb
@@ -1,21 +1,25 @@
 require_relative 'spec_helper.rb'
 
 module PrisonersDilemma
-class Generators
-  class << self
+  class Generators
+    class << self
 
-    INTEGER_MAX=(2**(0.size * 8 -2) -1) / 2 #copied from Rantly
+      INTEGER_MAX=(2**(0.size * 8 -2) -1) / 2 #copied from Rantly
 
-  def rules(max=INTEGER_MAX)
-    s = Rantly.range(0    , max - 3)
-    p = Rantly.range(s + 1, max - 2)
-    r = Rantly.range(r + 1, max - 1)
-    t = Rantly.range(c + 1, max)
-    Rantly.guard((2 * c) > (t + s))
-    data = Rules.new(t,r,p,s)
-    # TODO: add a shrink method to the returned object?
-    data
+      def rules(max=INTEGER_MAX)
+        rantly = Rantly.singleton
+        s = rantly.range(0    , max - 3)
+        #puts "s = #{s}"
+        p = rantly.range(s + 1, max - 2)
+        #puts "p = #{p}"
+        c = rantly.range(p + 1, max - 1)
+        #puts "r = #{r}"
+        t = rantly.range(c + 1, max)
+        rantly.guard((2 * c) > (t + s))
+        data = Rules.new(t,c,p,s)
+        # TODO: add a shrink method to the returned object?
+        data
+      end
+    end
   end
-  end
-end
 end
