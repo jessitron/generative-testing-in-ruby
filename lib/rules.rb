c2eef7f9d2632f01ea88fbd3714be0c913bfb83a
Mario Aquino <mario.e.aquino@gmail.com>
2014-08-05 19:07:33 -0500

    Refining

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,24 +1,23 @@
-
 module PrisonersDilemma
-class Rules
+  class Rules
 
-  def initialize (temptation_to_defect, reward_for_mutual_cooperation, punishment_for_mutual_defection, suckers_payout)
-    @t = temptation_to_defect
-    @c = reward_for_mutual_cooperation
-    @p = punishment_for_mutual_defection
-    @s = suckers_payout
-  end
+    def initialize (temptation_to_defect, reward_for_mutual_cooperation, punishment_for_mutual_defection, suckers_payout)
+      @t = temptation_to_defect
+      @c = reward_for_mutual_cooperation
+      @p = punishment_for_mutual_defection
+      @s = suckers_payout
+    end
 
-  def score(move1, move2)
-    [@c,@c]
-  end
+    def score(move1, move2)
+      [@c,@c]
+    end
 
-  def to_s
-    "Rules(t=#{@t}, c=#{@c}, p=#{@p}, s=#{@s})"
-  end
+    def to_s
+      "Rules(t=#{@t}, c=#{@c}, p=#{@p}, s=#{@s})"
+    end
 
-  def inspect
-    to_s
+    def inspect
+      to_s
+    end
   end
-end
 end
